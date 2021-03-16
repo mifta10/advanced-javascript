d6b40a6a34ef128dbd41f0beef052a666a7e4a8e
mifta10 <miftauddinkhan10@gmail.com>
2021-03-16 21:31:05 +0600

    nul,undefine,double tripple equal

---
+++ null-undefined.js
@@ -0,0 +1,17 @@
+let pakhi;
+console.log(pakhi);
+
+let c= undefined;
+console.log(c);
+
+// function add(n1,n2){
+//   console.log(n1 + n2);
+// }
+function add(n1,n2){
+  console.log(n1,n2);
+}
+const result = add(12);
+console.log(result);
+
+const ages =[13,214,43,5];
+console.log(ages[11]);
---
+++ true-false.js
@@ -0,0 +1,18 @@
+//falsy
+// false
+// 0
+// ""
+// undefined
+// null
+// NaN
+
+//Truthy
+// 1,2|" "|true,[],{}
+
+let name;
+if(name){
+  console.log("Condition is True");
+}
+else{
+  console.log("Condition is false");
+}
